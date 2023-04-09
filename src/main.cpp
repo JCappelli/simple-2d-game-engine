5cc9972f087300bb736aab03a5c6a11bbcd40807
JoshCappelli <josh.cappelli@gmail.com>
2023-04-08 23:29:46 -0400

    Implements all other libraies, std errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
+#include <string_view>
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
+    // sol::state lua;
+    //lua.open_libraries(sol::lib::base);
 
+    SDL_Init(SDL_INIT_EVERYTHING);
+    
     std::cout << "Hello World" << std::endl;
     return 0;
 }
