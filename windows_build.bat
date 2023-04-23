a393f9f1fbfaf0bcc9069e64744a4736bfcd759c
JoshCappelli <josh.cappelli@gmail.com>
2023-04-22 20:34:43 -0400

    Organizes Subfolders and reworks build system

--- windows_build.bat
+++ windows_build.bat
@@ -12,18 +12,26 @@ mkdir build
 
 ::Compile
 :compile
-ECHO Compiling Project
+ECHO Starting Build
+ECHO Comiling Source
+cd build
+
+::Compile Cpp files
+for /r ..\src %%s in (*.cpp) do (
+ g++ -c -std=c++17 -Wall -I..\lib\SDL\Windows\include -I..\lib\Lua\Windows\include -I..\lib\ -I..\lib\sol %%s -g
+)
+
+cd ../
+
+::Linking Step
+ECHO Linking Compiled Code
 g++^
  -std=c++17^
  -Wall^
- -Ilib\SDL\Windows\include^
- -Ilib\Lua\Windows\include^
- -Ilib\^
- -Ilib\sol^
  -Llib\SDL\Windows\lib^
  -Llib\Lua\Windows^
  -o build\game.exe^
- src\*.cpp^
+ build\*.o^
  -lmingw32^
  -lSDL2main^
  -lSDL2^
@@ -32,7 +40,9 @@ g++^
  -lSDL2_mixer^
  -llua54^
  -g
-::Last Arg is DEBUG FLAG
+::Last Arg is DEBUG FLAG "-g"
+
+del build\*.o
 
 if errorlevel 0 goto :postbuild
 
