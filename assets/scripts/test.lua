52f8bdf6258a976decb7f0807c9219e945df92c8
Joshua Cappelli <josh.cappelli@gmail.com>
2023-08-15 20:14:44 -0400

    Tests Loading Lua script and reading from script

---
+++ assets/scripts/test.lua
@@ -0,0 +1,7 @@
+--This is a lua test script
+
+Basic_variable = 7 * 6
+
+if Basic_variable == 42 then
+    print("The Value of the variable is "..Basic_variable)
+end
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <sol/sol.hpp>
+
 #include "Game/Game.h"
+
+void TestLua()
+{
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
+    lua.script_file("./assets/scripts/test.lua");
+
+    int luaVariable = lua["Basic_variable"];
+    std::cout << "The Value is " << luaVariable << std::endl;
+    std::cin.get();
+}
 
 int main(int argc, char *argv[])
 {
-    std::unique_ptr<Game> game = std::make_unique<Game>();
+    TestLua();
 
-    game->Initialize();
-    game->Run();
-    game->Destroy();
+    // std::unique_ptr<Game> game = std::make_unique<Game>();
+
+    // game->Initialize();
+    // game->Run();
+    // game->Destroy();
 
     return 0;
 }
