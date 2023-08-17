7db346fc1c1cb55cac455fb5efd4f8f39ba0b8ed
Joshua Cappelli <josh.cappelli@gmail.com>
2023-08-17 19:40:56 -0400

    Reorganizes level files

---
+++ tiled/tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tilemap" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+ <image source="../assets/sprites/mini-dungeon-tiles.png" width="192" height="176"/>
+</tileset>
