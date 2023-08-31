7c13dc3fccb9289690d9db4a56a218350990afbf
JCappelli <josh.cappelli@gmail.com>
2023-08-30 20:41:29 -0400

    Adds Support for Mac Builds via Xcode/Premake

--- premake5.lua
+++ premake5.lua
@@ -12,6 +12,64 @@ project "Simple2DGameEngine"
     targetdir ("build")
     debugdir ("build")
 
+    files
+    {
+        "src/**.cpp",
+        "src/**.h",
+        "lib/imgui/*.cpp",
+        "lib/imgui/*.h"
+    }
+
+filter "configurations:Debug"
+    defines { "DEBUG" }
+    symbols "On"
+
+filter "configurations:Release"
+    defines { "NDEBUG" }
+    optimize "On"
+
+filter "system:macosx"
+    externalincludedirs
+    {
+        "lib/SDL/Mac/SDL2.framework/Headers",
+        "lib/SDL/Mac/SDL2_image.framework/Headers",
+        "lib/SDL/Mac/SDL2_ttf.framework/Headers",
+        "lib/lua/include",
+        "lib/sol",
+        "lib/glm",
+        "lib/imgui"
+    }
+
+    frameworkdirs
+    {
+        "lib/SDL/Mac"
+    }
+
+    links
+    {
+        "lib/SDL/Mac/SDL2.framework",
+        "lib/SDL/Mac/SDL2_image.framework",
+        "lib/SDL/Mac/SDL2_ttf.framework"
+    }
+
+    embedAndSign 
+    {
+        "SDL2.framework",
+        "SDL2_image.framework",
+        "SDL2_ttf.framework"
+    }
+
+    xcodebuildsettings
+    {
+        ["OTHER_LDFLAGS"] = "../lib/lua/MacLib/liblua.a"
+    }
+
+    postbuildcommands
+    {
+        "{COPYDIR} %{wks.location}/../assets %{cfg.targetdir}"
+    }
+
+filter "system:windows"
     includedirs
     {
         "lib/SDL/Windows/include",
@@ -35,25 +93,9 @@ project "Simple2DGameEngine"
         "lua54"
     }
 
-    files
+    postbuildcommands
     {
-        "src/**.cpp",
-        "src/**.h",
-        "lib/imgui/*.cpp",
-        "lib/imgui/*.h"
+        "{COPYFILE} %{wks.location}/../lib/SDL/Windows/lib/*.dll %{cfg.targetdir}",
+        "{MKDIR} %{cfg.targetdir}/assets",
+        "{COPYDIR} %{wks.location}/../assets %{cfg.targetdir}/assets"
     }
-
-postbuildcommands
-{
-    "{COPYFILE} %{wks.location}/../lib/SDL/Windows/lib/*.dll %{cfg.targetdir}",
-    "{MKDIR} %{cfg.targetdir}/assets",
-    "{COPYDIR} %{wks.location}/../assets %{cfg.targetdir}/assets"
-}
-
-filter "configurations:Debug"
-    defines { "DEBUG" }
-    symbols "On"
-
- filter "configurations:Release"
-    defines { "NDEBUG" }
-    optimize "On"
